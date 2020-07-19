5e91cd7f49e529e53cb606168580fa63e2686c50
jasonngyt <jasonngyt@gmail.com>
2020-07-20 00:27:35 +0800

    Sorting Extracting a Method

--- src/components/moviesTable.js
+++ src/components/moviesTable.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import Table from '/common/table';
+import Table from './common/table';
 import Like from './common/like';
 
 class MoviesTable extends Component {
