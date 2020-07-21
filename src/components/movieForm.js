fd9c75e54d69439e523b1397e3b65fc4d2f968db
jasonngyt <jasonngyt@gmail.com>
2020-07-22 00:06:47 +0800

    Add NavBar and link Movie Form

--- src/components/movieForm.js
+++ src/components/movieForm.js
@@ -1,7 +1,17 @@
 import React from 'react';
 
-const MovieForm = () => {
-  return <h1>Movie Form</h1>;
+const MovieForm = ({ match, history }) => {
+  return (
+    <div>
+      <h1>Movie Form {match.params.id}</h1>;
+      <buttton
+        className='btn btn-primary'
+        onClick={() => history.push('/movies')}
+      >
+        Save
+      </buttton>
+    </div>
+  );
 };
 
 export default MovieForm;
