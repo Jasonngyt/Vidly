4900c0dd395ce19566ede08364c204e3400452c6
jasonngyt <jasonngyt@gmail.com>
2020-07-22 21:37:21 +0800

    Using Refs

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
 
 class loginForm extends Component {
+  username = React.createRef();
+
+  //   componentDidMount() {
+  //     this.username.current.focus();
+  //   }
+
   handleSubmit = (e) => {
     e.preventDefault();
 
+    const username = this.username.current.value;
     console.log('Submitted');
   };
   render() {
@@ -13,7 +20,13 @@ class loginForm extends Component {
         <form onSubmit={this.handleSubmit}>
           <div className='form-group'>
             <label htmlFor='username'>Username</label>
-            <input id='username' type='text' className='form-control' />
+            <input
+              autofocus
+              ref={this.username}
+              id='username'
+              type='text'
+              className='form-control'
+            />
           </div>
           <div className='form-group'>
             <label htmlFor='password'>Password</label>
