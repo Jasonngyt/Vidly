45392aa0f6878327a43fb4a4f3942af461e7d6d5
jasonngyt <jasonngyt@gmail.com>
2020-07-23 18:32:43 +0800

    Displaying Validation Errors

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -37,7 +37,7 @@ class loginForm extends Component {
   };
 
   render() {
-    const { account } = this.state;
+    const { account, errors } = this.state;
 
     return (
       <div>
@@ -48,12 +48,14 @@ class loginForm extends Component {
             value={account.username}
             label='Username'
             onChange={this.handleChange}
+            error={errors.username}
           />
           <Input
             name='password'
             value={account.password}
             label='Password'
             onChange={this.handleChange}
+            error={errors.password}
           />
 
           <button className='btn-primary'>Login</button>
