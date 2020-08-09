e6ce316bdcde5a2b13c8ae8b3a8cab64d2c3c743
jasonngyt <jasonngyt@gmail.com>
2020-08-09 19:00:35 +0800

    Adding Http and Log Serivces

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Route, Redirect, Switch } from 'react-router-dom';
+import { ToastContainer } from 'react-toastify';
 import Movies from './components/movies';
 import Customers from './components/customers';
 import Rentals from './components/rentals';
@@ -7,12 +8,14 @@ import MovieForm from './components/movieForm';
 import NotFound from './components/notFound';
 import NavBar from './components/navBar';
 import LoginForm from './components/loginForm';
+import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <React.Fragment>
+        <ToastContainer />
         <NavBar />
         <main className='container'>
           <Switch>
