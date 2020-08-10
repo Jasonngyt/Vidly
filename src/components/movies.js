55eb4cf4f77226902599e29ec1f4c9c042f899df
jasonngyt <jasonngyt@gmail.com>
2020-08-10 15:59:59 +0800

    Replace FakeGenreService with actual api

--- src/components/movies.js
+++ src/components/movies.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import ListGroup from './common/listGroup';
 import Pagination from './common/pagination';
 import { getMovies } from '../services/fakeMovieService';
-import { getGenres } from '../services/fakeGenreService';
+import { getGenres } from '../services/genreService';
 import { paginate } from '../utils/paginate';
 import MoviesTable from './moviesTable';
 import _ from 'lodash';
@@ -20,8 +20,10 @@ class Movies extends Component {
     sortColumn: { path: 'title', order: 'asc' },
   };
 
-  componentDidMount() {
-    const genres = [{ _id: '', name: 'All Genres' }, ...getGenres()];
+  async componentDidMount() {
+    const { data } = await getGenres();
+
+    const genres = [{ _id: '', name: 'All Genres' }, ...data];
 
     this.setState({ movies: getMovies(), genres });
   }
