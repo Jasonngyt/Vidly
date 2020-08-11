45e88a14ff913177cba35764c587f126cfc82291
jasonngyt <jasonngyt@gmail.com>
2020-08-11 11:55:30 +0800

    Submit Registration Form

--- src/App.js
+++ src/App.js
@@ -8,6 +8,7 @@ import MovieForm from './components/movieForm';
 import NotFound from './components/notFound';
 import NavBar from './components/navBar';
 import LoginForm from './components/loginForm';
+import RegisterForm from './components/registerForm';
 import 'react-toastify/dist/ReactToastify.css';
 import './App.css';
 
@@ -20,6 +21,7 @@ class App extends Component {
         <main className='container'>
           <Switch>
             <Route path='/login' component={LoginForm} />
+            <Route path='/register' component={RegisterForm} />
             <Route path='/movies/:id' component={MovieForm} />
             <Route path='/movies' component={Movies} />
             <Route path='/customers' component={Customers} />
