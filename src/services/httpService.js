2cb7f95195a78becf08f4c3e78602a4a09d768a6
jasonngyt <jasonngyt@gmail.com>
2020-08-11 19:43:44 +0800

    Fixing Bidirectional Dependecies

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 import logger from './logService';
-import auth from './authService';
-import { toast } from 'react-toastify';
 
-axios.defaults.headers.common['x-auth-token'] = auth.getJwt();
+import { toast } from 'react-toastify';
 
 axios.interceptors.response.use(null, (error) => {
   const expectedError =
@@ -17,9 +15,14 @@ axios.interceptors.response.use(null, (error) => {
   return Promise.reject(error);
 });
 
+function setJwt(jwt) {
+  axios.defaults.headers.common['x-auth-token'] = jwt;
+}
+
 export default {
   get: axios.get,
   post: axios.post,
   put: axios.put,
   delete: axios.delete,
+  setJwt,
 };
